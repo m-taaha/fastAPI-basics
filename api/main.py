89b7c0e9fb90a72210a4a88051a6606cb9e327a4
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-11 23:29:56 +0530

    feat: add config settings for db connection

--- api/main.py
+++ api/main.py
@@ -1,3 +1,7 @@
-from fastapi import Fastapi
+from fastapi import FastAPI
 
-app = Fastapi(title = "This is my Task Management Application")
+app = FastAPI(title = "This is my Task Management Application")
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI server running"}
